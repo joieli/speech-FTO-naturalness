a6320212d79db4a75bd98feafe91028696fa5e12
Joie Li <g246li@uwaterloo.ca>
2026-04-01 12:54:37 +0200

    adding the ability to test both absolute offsets and the base offset

--- run_pilot.m
+++ run_pilot.m
@@ -25,10 +25,13 @@ rng(42);
 audiopath = "audio_clips";
 clipNames = ["F1F2_quiet_food_clip01","F1F2_quiet_food_clip05","F1F2_quiet_food_clip07","F1F2_quiet_food_clip08"...
     ,"F1F2_quiet_food_clip11", "F1F2_quiet_food_clip13", "F1F2_quiet_food_clip14", "F1F2_quiet_food_clip17"];     % audio clips to use, access with clipNames(clipIdx)
+clipNames = ["F1F2_quiet_food_clip01","F1F2_quiet_food_clip05","F1F2_quiet_food_clip07"];
 resultspath = "results";
 %parameters
-abs_offsets = [0,2000];             %absolute_offsets in ms, acesss with abs_offsets(offsetIdx)
-reps_per_offset_per_clip = 1;                                       % how many repetitions of each offset to present the participant                        
+abs_offsets = {0,750,1500,2250,3000,"base"};             %absolute_offsets in ms, acesss with abs_offsets{offsetIdx} - curly brace because cell, add "base" on the end to also test the no aritificial offset condition
+abs_offsets = {0,500,1000,1500,2000,"base"};
+abs_offsets = {0,1000,"base"};
+reps_per_offset_per_clip = 3;                                       % how many repetitions of each offset to present the participant                        
 timeMe = true;
 %training
 runTrain = true;
@@ -49,7 +52,8 @@ rel_offsets = zeros(n_clips,n_offsets);                                     %acc
 
 
 % create tables to store data
-raw_results_table = zeros(n_clips,n_offsets,reps_per_offset_per_clip);      %access with raw_results_table(clipIdx, offsetIdx,repIdx) - each table is a repetition, each row is a clip, each col is a different offset
+raw_results_table = zeros(n_clips,n_offsets,reps_per_offset_per_clip);    %access with raw_results_table(clipIdx, offsetIdx,repIdx) - each table is a repetition, each row is a clip, each col is a different offset 
+                                                                            %access the base offset with raw_results_table(clipIdx, end,repIdx)
 raw_results_time(n_trials) = Trial;                                         %access with raw_results_time(idx).property
 
 %% Load audios
@@ -57,7 +61,11 @@ for clipIdx = 1:n_clips
     clips(clipIdx) = load_audio_clip(audiopath, clipNames(clipIdx), true);
     base_offsets(clipIdx) = getBaseOffset(audiopath,clipNames(clipIdx));
     clips(clipIdx).base_offset = base_offsets(clipIdx);
-    rel_offsets(clipIdx, :) = abs_offsets-base_offsets(clipIdx);
+
+    %leave the last element as a zero relative offset (represents no shift)
+    for offsetIdx = 1:n_offsets-1
+        rel_offsets(clipIdx, offsetIdx) = abs_offsets{offsetIdx}-base_offsets(clipIdx);
+    end
 end
 
 %% get clip order
@@ -87,7 +95,7 @@ for idx = 1:n_trials
     raw_results_time(idx).clipName = clipNames(clipIdx);
     raw_results_time(idx).offsetIdx = offsetIdx;
     raw_results_time(idx).relOffset = rel_offsets(clipIdx, offsetIdx);
-    raw_results_time(idx).absOffset = abs_offsets(offsetIdx);
+    raw_results_time(idx).absOffset = abs_offsets{offsetIdx};
     raw_results_time(idx).baseOffset = base_offsets(clipIdx);
     raw_results_time(idx).repIdx = repIdx;
 
